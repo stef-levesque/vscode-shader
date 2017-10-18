d1ecc226254ab3d54d236d74214514dd249ff3fc
Stephane Levesque <stef.levesque@gmail.com>
2017-10-18 09:22:05 -0400

    make SymbolProvider match more texture and buffer

--- src/hlsl/symbolProvider.ts
+++ src/hlsl/symbolProvider.ts
@@ -5,7 +5,8 @@ import { DocumentSymbolProvider, SymbolKind, SymbolInformation, CancellationToke
 const functionMatch = /^\w+\s+([a-zA-Z_\x7f-\xff][a-zA-Z0-9:_\x7f-\xff]*)\s*\(/mg; 
 const structMatch = /^(?:struct|cbuffer|tbuffer)\s+([a-zA-Z_\x7f-\xff][a-zA-Z0-9:_\x7f-\xff]*)/mg;
 const samplerMatch = /^(?:sampler|sampler1D|sampler2D|sampler3D|samplerCUBE|samplerRECT|sampler_state|SamplerState)\s+([a-zA-Z_\x7f-\xff][a-zA-Z0-9:_\x7f-\xff]*)/mg;
-const textureMatch = /^(?:texture|texture2D|textureCUBE|Texture1D|Texture1DArray|Texture2D|Texture2DArray|Texture2DMS|Texture2DMSArray|Texture3D|TextureCube|TextureCubeArray)(?:\s*<(?:[a-zA-Z_\x7f-\xff][a-zA-Z0-9,_\x7f-\xff]*)>)?\s+([a-zA-Z_\x7f-\xff][a-zA-Z0-9\[\]_\x7f-\xff]*)/mg;
+const textureMatch = /^(?:texture|texture2D|textureCUBE|Texture1D|Texture1DArray|Texture2D|Texture2DArray|Texture2DMS|Texture2DMSArray|Texture3D|TextureCube|TextureCubeArray|RWTexture1D|RWTexture1DArray|RWTexture2D|RWTexture2DArray|RWTexture3D)(?:\s*<(?:[a-zA-Z_\x7f-\xff][a-zA-Z0-9,_\x7f-\xff]*)>)?\s+([a-zA-Z_\x7f-\xff][a-zA-Z0-9\[\]_\x7f-\xff]*)/mg;
+const bufferMatch = /^(?:AppendStructuredBuffer|Buffer|ByteAddressBuffer|ConsumeStructuredBuffer|RWBuffer|RWByteAddressBuffer|RWStructuredBuffer|StructuredBuffer)(?:\s*<(?:[a-zA-Z_\x7f-\xff][a-zA-Z0-9,_\x7f-\xff]*)>)?\s+([a-zA-Z_\x7f-\xff][a-zA-Z0-9\[\]_\x7f-\xff]*)/mg;
 
 export default class HLSLDocumentSymbolProvider implements DocumentSymbolProvider {
 
@@ -41,6 +42,7 @@ export default class HLSLDocumentSymbolProvider implements DocumentSymbolProvide
             fetchSymbol(structMatch, SymbolKind.Struct);
             fetchSymbol(samplerMatch, SymbolKind.Variable);
             fetchSymbol(textureMatch, SymbolKind.Field);
+            fetchSymbol(bufferMatch, SymbolKind.Field);
 
             resolve(result);
 
