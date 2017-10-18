d0dd033e79473a677b85d9fef9350b5a5be02165
Stephane Levesque <stef.levesque@gmail.com>
2017-10-18 09:42:25 -0400

    :sparkles: open HLSL documentation on the side

--- src/hlsl/hoverProvider.ts
+++ src/hlsl/hoverProvider.ts
@@ -1,7 +1,11 @@
 'use strict';
 
-import { HoverProvider, Hover, SymbolInformation, SymbolKind, MarkdownString, MarkedString, TextDocument, CancellationToken, Range, Position, commands, workspace } from 'vscode';
+import { TextDocumentContentProvider, HoverProvider, Hover, SymbolInformation, SymbolKind, MarkdownString, MarkedString, TextDocument, CancellationToken, Range, Position, Uri, ViewColumn, Disposable, commands, workspace } from 'vscode';
 import hlslGlobals = require('./hlslGlobals');
+import * as https from 'https';
+
+const jsdom = require('jsdom');
+const { JSDOM } = jsdom;
 
 const linktext: string = "[HLSL documentation][1]\n\n[1]: ";
 
@@ -11,8 +15,25 @@ export function textToMarkedString(text: string): MarkedString {
 
 export default class HLSLHoverProvider implements HoverProvider {
 
+    private _subscriptions: Disposable[] = [];
+
     private getSymbols(document: TextDocument): Thenable<SymbolInformation[]> {
         return commands.executeCommand<SymbolInformation[]>('vscode.executeDocumentSymbolProvider', document.uri);
+    }
+
+
+
+    constructor() {
+        this._subscriptions.push( commands.registerCommand('shader.openLink', (link: Uri | string) => {
+            let hlsldoclink =  Uri.parse(link.toString()).with({scheme: 'hlsldoc'}); 
+            commands.executeCommand('vscode.previewHtml', hlsldoclink, ViewColumn.Two, "HLSL Documentation");
+        }));
+
+        this._subscriptions.push( workspace.registerTextDocumentContentProvider('hlsldoc', new HLSLDocumentationTextDocumentProvider() ) );
+    }
+
+    dispose() {
+        this._subscriptions.forEach(s => {s.dispose()});
     }
 
     public async provideHover(document: TextDocument, position: Position, token: CancellationToken): Promise<Hover> {
@@ -46,7 +67,8 @@ export default class HLSLHoverProvider implements HoverProvider {
 
                 if (entry.link && entry.link != '') {
                     let link = new MarkdownString(linktext);
-                    link.appendText(entry.link);
+                    //link.appendText(entry.link);
+                    link.appendText(encodeURI('command:shader.openLink?' + JSON.stringify([entry.link])))
                     link.isTrusted = true;
                     contents.push(link);
                 }
@@ -71,7 +93,8 @@ export default class HLSLHoverProvider implements HoverProvider {
 
             if (entry.link && entry.link != '') {
                 let link = new MarkdownString(linktext);
-                link.appendText(entry.link);
+                //link.appendText(entry.link);
+                link.appendText(encodeURI('command:shader.openLink?' + JSON.stringify([entry.link])))
                 link.isTrusted = true;
                 contents.push(link);
             }
@@ -88,7 +111,8 @@ export default class HLSLHoverProvider implements HoverProvider {
 
             if (entry.link && entry.link != '') {
                 let link = new MarkdownString(linktext);
-                link.appendText(entry.link);
+                //link.appendText(entry.link);
+                link.appendText(encodeURI('command:shader.openLink?' + JSON.stringify([entry.link])))
                 link.isTrusted = true;
                 contents.push(link);
             }
@@ -105,7 +129,8 @@ export default class HLSLHoverProvider implements HoverProvider {
 
             if (entry.link && entry.link != '') {
                 let link = new MarkdownString(linktext);
-                link.appendText(entry.link);
+                //link.appendText(entry.link);
+                link.appendText(encodeURI('command:shader.openLink?' + JSON.stringify([entry.link])))
                 link.isTrusted = true;
                 contents.push(link);
             }
@@ -123,7 +148,8 @@ export default class HLSLHoverProvider implements HoverProvider {
 
             if (entry.link && entry.link != '') {
                 let link = new MarkdownString(linktext);
-                link.appendText(entry.link);
+                //link.appendText(entry.link);
+                link.appendText(encodeURI('command:shader.openLink?' + JSON.stringify([entry.link])))
                 link.isTrusted = true;
                 contents.push(link);
             }
@@ -140,7 +166,8 @@ export default class HLSLHoverProvider implements HoverProvider {
 
             if (entry.link && entry.link != '') {
                 let link = new MarkdownString(linktext);
-                link.appendText(entry.link);
+                //link.appendText(entry.link);
+                link.appendText(encodeURI('command:shader.openLink?' + JSON.stringify([entry.link])))
                 link.isTrusted = true;
                 contents.push(link);
             }
@@ -167,4 +194,245 @@ export default class HLSLHoverProvider implements HoverProvider {
             }
         }
     } 
+}
+
+class HLSLDocumentationTextDocumentProvider implements TextDocumentContentProvider {
+
+    public provideTextDocumentContent(uri): Promise<string> {
+        return new Promise<string>((resolve, reject) => { 
+            let request = https.request({
+                host: uri.authority,
+                path: uri.path,
+                rejectUnauthorized: workspace.getConfiguration().get("http.proxyStrictSSL", true)
+            }, (response) => {
+                if (response.statusCode == 301 || response.statusCode == 302)
+                    return resolve(response.headers.location);
+                if (response.statusCode != 200)
+                    return resolve(response.statusCode.toString());
+                let topic = '';
+                response.on('data', (data) => { topic += data.toString(); });
+                response.on('end', () => { 
+                    const dom = new JSDOM(topic);
+                    let node = dom.window.document.querySelector('.topic');
+                    topic = node.outerHTML;
+
+                    resolve(`
+                    <!DOCTYPE html>
+                    <html>
+                    
+                    <head>
+                        <STYLE type="text/css">
+                            body {
+                                padding: 10px 20px;
+                                line-height: 22px
+                            }
+                    
+                            img {
+                                max-width: 100%;
+                                max-height: 100%
+                            }
+                    
+                            a {
+                                text-decoration: none
+                            }
+                    
+                            a:focus,
+                            input:focus,
+                            select:focus,
+                            textarea:focus {
+                                outline: 1px solid -webkit-focus-ring-color;
+                                outline-offset: -1px
+                            }
+                    
+                            hr {
+                                border: 0;
+                                height: 2px;
+                                border-bottom: 2px solid
+                            }
+                    
+                            h1 {
+                                padding-bottom: .3em;
+                                line-height: 1.2;
+                                border-bottom-width: 1px;
+                                border-bottom-style: solid
+                            }
+                    
+                            h1,
+                            h2,
+                            h3 {
+                                font-weight: 400
+                            }
+                    
+                            a:hover {
+                                text-decoration: underline
+                            }
+                    
+                            table {
+                                border-collapse: collapse
+                            }
+                    
+                            table > thead > tr > th {
+                                text-align: left
+                            }
+                    
+                            table > thead > tr > th,
+                            table > thead > tr > td,
+                            table > tbody > tr > th,
+                            table > tbody > tr > td {
+                                padding: 5px 10px
+                            }
+                    
+                            blockquote {
+                                margin: 0 7px 0 5px;
+                                padding: 0 16px 0 10px;
+                                border-left: 5px solid
+                            }
+                    
+                            code {
+                                font-family: Menlo, Monaco, Consolas, "Droid Sans Mono", "Courier New", monospace, "Droid Sans Fallback";
+                                font-size: 14px;
+                                line-height: 19px
+                            }
+                    
+                            .mac code {
+                                font-size: 12px;
+                                line-height: 18px
+                            }
+                    
+                            code > div {
+                                padding: 16px;
+                                border-radius: 3px;
+                                overflow: auto
+                            }
+                    
+                            .monaco-tokenized-source {
+                                white-space: pre
+                            }
+                    
+                            /** Theming */
+                    
+                            .vscode-light {
+                                color: #1e1e1e
+                            }
+                    
+                            .vscode-dark {
+                                color: #ddd
+                            }
+                    
+                            .vscode-high-contrast {
+                                color: #fff
+                            }
+                    
+                            .vscode-light code {
+                                color: #a31515
+                            }
+                    
+                            .vscode-dark code {
+                                color: #d7ba7d
+                            }
+                    
+                            .vscode-light code > div {
+                                background-color: rgba(220, 220, 220, .4)
+                            }
+                    
+                            .vscode-dark code > div {
+                                background-color: rgba(10, 10, 10, .4)
+                            }
+                    
+                            .vscode-high-contrast code > div {
+                                background-color: #000
+                            }
+                    
+                            .vscode-high-contrast h1 {
+                                border-color: #000
+                            }
+                    
+                            .vscode-light table > thead > tr > th {
+                                border-color: rgba(0, 0, 0, .69)
+                            }
+                    
+                            .vscode-dark table > thead > tr > th {
+                                border-color: rgba(255, 255, 255, .69)
+                            }
+                    
+                            .vscode-light h1,
+                            .vscode-light hr,
+                            .vscode-light table > tbody > tr + tr > td {
+                                border-color: rgba(0, 0, 0, .18)
+                            }
+                    
+                            .vscode-dark h1,
+                            .vscode-dark hr,
+                            .vscode-dark table > tbody > tr + tr > td {
+                                border-color: rgba(255, 255, 255, 0.18)
+                            }
+                    
+                            .vscode-light blockquote,
+                            .vscode-dark blockquote {
+                                background: rgba(127, 127, 127, .1);
+                                border-color: rgba(0, 122, 204, .5)
+                            }
+                    
+                            .vscode-high-contrast blockquote {
+                                background: transparent;
+                                border-color: #fff
+                            }
+                    
+                            .main-content {
+                                width: 65%;
+                                vertical-align: top
+                            }
+                    
+                            .sidebar {
+                                vertical-align: top
+                            }
+                    
+                            .footer {
+                                padding: 25px;
+                                text-align: center
+                            }
+                    
+                            .vscode-light table > tbody > tr > td.sidebar {
+                                background-color: rgba(0, 0, 0, 0.1);
+                                border-radius: 10px
+                            }
+                    
+                            .vscode-dark table > tbody > tr > td.sidebar {
+                                background-color: rgba(255, 255, 255, 0.1);
+                                border-radius: 10px
+                            }
+                    
+                            .vscode-light a {
+                                color: #4080D0
+                            }
+                    
+                            .vscode-dark a {
+                                color: #a2c1e8
+                            }
+
+                            .vscode-high-contrast .codeSnippetContainerCode div {
+                                color: black !important
+                            }
+
+                            .vscode-light .codeSnippetContainerCode div {
+                                color: black !important
+                            }
+
+                            .vscode-dark .codeSnippetContainerCode div {
+                                color: white !important
+                            }
+                        </STYLE>
+                    </head>
+                    <body>
+                    ${topic}
+                    </body>
+                    </html>
+                    `);
+                });
+                response.on('error', (error) => { console.log(error); });
+            });
+            request.on('error', (error) => { console.log(error) });
+            request.end();
+        });
+    }
 }
