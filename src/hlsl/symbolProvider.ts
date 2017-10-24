47507350b4426ccda8d78a1fd33d8052f4faa2a6
Stephane Levesque <stef.levesque@gmail.com>
2017-10-24 01:52:37 -0400

    :ambulance: remove faulty symbol caching and let ripgrep do the file matching

--- src/hlsl/symbolProvider.ts
+++ src/hlsl/symbolProvider.ts
@@ -19,29 +19,20 @@ export interface ISymbolCache { [path: string]: SymbolInformation[]; }
 
 export default class HLSLDocumentSymbolProvider implements DocumentSymbolProvider, WorkspaceSymbolProvider {
 
-    private _symbolCache: ISymbolCache;
     private _disposables: Disposable[] = [];
 
-    private _hlslPattern = '{**/*.hlsl,**/*.hlsli,**/*.fx,**/*.fxh,**/*.vsh,**/*.psh,**/*.cginc,**/*.compute}';
+    private _hlslPattern = ['.hlsl','.hlsli','.fx','.fxh','.vsh','.psh','.cginc','.compute'];
     
     constructor() {
-        this._symbolCache = {};
-
         const extention = extensions.getExtension('vscode.hlsl');
         if (extention && extention.packageJSON 
             && extention.packageJSON.contributes
             && extention.packageJSON.contributes.languages) {
             let hlsllang: any[] = extention.packageJSON.contributes.languages.filter(l => l.id === 'hlsl');
             if (hlsllang.length && hlsllang[0].extensions) {
-                this._hlslPattern = '{**/*' + hlsllang[0].extensions.join(',**/*') + '}';
+                this._hlslPattern = hlsllang[0].extensions;
             }
         }
-
-        // watch files to invalidate cache, if needed
-        let watcher = workspace.createFileSystemWatcher(new RelativePattern(workspace.rootPath, this._hlslPattern), true);
-        watcher.onDidChange(uri => { this._symbolCache[uri.fsPath] = undefined; console.log(uri.fsPath); });
-        watcher.onDidDelete(uri => { this._symbolCache[uri.fsPath] = undefined; console.log(uri.fsPath); });
-        this._disposables.push(watcher);
     }
 
     public dispose(){
@@ -111,64 +102,41 @@ export default class HLSLDocumentSymbolProvider implements DocumentSymbolProvide
     public provideWorkspaceSymbols(query: string, token: CancellationToken): Thenable<SymbolInformation[]> {
         return new Promise<SymbolInformation[]>((resolve, reject) => {
 
-            workspace.findFiles(this._hlslPattern).then(uris => {
-                let files = uris.reduce((a,b) => {
-                    if (!this._symbolCache[b.fsPath]) {
-                        this._symbolCache[b.fsPath] = [];
-                        a += ' ' + b.fsPath;
-                    }
-                    return a;
-                }, '');
+            const execOpts = {
+                cwd: workspace.rootPath,
+                maxBuffer: 1024 * 1024
+            }
 
-                if (files !== '') {
-                    const execOpts = {
-                        cwd: workspace.rootPath,
-                        maxBuffer: 1024 * 1024
-                    }
-                    
-                    for (let entry of searchPatterns) {
-                        const kind = entry.kind;
-                        const searchPattern = entry.pattern;
-                        let output = execSync(`${rgPath} -o --case-sensitive --line-number --column --hidden -e "${searchPattern}" ${files}`, execOpts);
+            let results: SymbolInformation[] = [];
+            let includePattern = '-g *' +  this._hlslPattern.join(' -g *'); 
 
-                        let lines = output.toString().split('\n');
-                        for (let line of lines) {
-                            let lineMatch = /^(?:((?:[a-zA-Z]:)?[^:]*):)?(\d+):(\d):(.+)/.exec(line);
-                            if (lineMatch) {
-                                let position: Position = new Position(parseInt(lineMatch[2]) - 1, parseInt(lineMatch[3]) - 1);
-                                let range = new Range(position, position);
-                                let filepath = lineMatch[1] ? lineMatch[1] : files.trim();//join(workspace.rootPath, match[1]);
-                                let regex = new RegExp(searchPattern);
-                                let word = '?????';
-                                let symbolMatch = regex.exec(lineMatch[4].toString());
-                                if (symbolMatch) {
-                                    word = symbolMatch[1];
-                                    position = position.with({character: symbolMatch[0].indexOf(word)});
-                                    range = new Range(position, position.translate(0, word.length));
-                                }
+            for (let entry of searchPatterns) {
+                const kind = entry.kind;
+                const searchPattern = entry.pattern;
+                let output = execSync(`${rgPath} ${includePattern} -o --case-sensitive -H --line-number --column --hidden -e "${searchPattern}" .`, execOpts);
 
-                                if (!this._symbolCache[filepath]) {
-                                    console.log('missing: ' + filepath);
-                                    this._symbolCache[filepath] = [];
-                                }
-
-                                this._symbolCache[filepath].push(new SymbolInformation(word, kind, '', new Location(Uri.file(filepath), range)));
-                            }
+                let lines = output.toString().split('\n');
+                for (let line of lines) {
+                    let lineMatch = /^(?:((?:[a-zA-Z]:)?[^:]*):)?(\d+):(\d):(.+)/.exec(line);
+                    if (lineMatch) {
+                        let position: Position = new Position(parseInt(lineMatch[2]) - 1, parseInt(lineMatch[3]) - 1);
+                        let range = new Range(position, position);
+                        let filepath = join(workspace.rootPath, lineMatch[1]);
+                        let regex = new RegExp(searchPattern);
+                        let word = '?????';
+                        let symbolMatch = regex.exec(lineMatch[4].toString());
+                        if (symbolMatch) {
+                            word = symbolMatch[1];
+                            position = position.with({ character: symbolMatch[0].indexOf(word) });
+                            range = new Range(position, position.translate(0, word.length));
                         }
-                    }
 
-                }
-
-                let results: SymbolInformation[] = [];
-                for (let key in this._symbolCache) {
-                    if(this._symbolCache[key]) {
-                        results.push(...this._symbolCache[key]);
+                        results.push(new SymbolInformation(word, kind, '', new Location(Uri.file(filepath), range)));
                     }
                 }
+            }
 
-                resolve( results );
-
-            })
+            resolve( results );
         });
 
     }
